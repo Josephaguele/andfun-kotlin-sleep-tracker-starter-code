454f471acb903bd848790eb3ddcc037beaaf5c4a
Joseph <osekuemen@gmail.com>
2021-07-30 20:52:06 +0100

    Step 25 - correction of null error

--- app/src/main/java/com/example/android/trackmysleepquality/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/BindingUtils.kt
@@ -10,17 +10,18 @@ import com.example.android.trackmysleepquality.database.SleepNight
 
 //Step 1: For the ImageView, move the code from SleepNightAdapter.ViewHolder bind() function into an extension function:
 @BindingAdapter("sleepImage")
-fun ImageView.setSleepImage(item: SleepNight)
-{
-    setImageResource(when (item.sleepQualityRating) {
-        0 -> R.drawable.ic_sleep_0
-        1 -> R.drawable.ic_sleep_1
-        2 -> R.drawable.ic_sleep_2
-        3 -> R.drawable.ic_sleep_3
-        4 -> R.drawable.ic_sleep_4
-        5 -> R.drawable.ic_sleep_5
-        else -> R.drawable.ic_sleep_active
-    })
+fun ImageView.setSleepImage(item: SleepNight?) {
+    item?.let {
+        setImageResource(when (item.sleepQualityRating) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
 }
 // Step 2: For the two TextViews, convert the data to formatted text using the functions provided in Util.kt:
 @BindingAdapter("sleepDurationFormatted")
